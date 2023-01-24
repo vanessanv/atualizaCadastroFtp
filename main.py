d63e20f92f28cee659b9af43a6becb843adac2f1
Vanessa Viana <39503782+vanessanv@users.noreply.github.com>
2023-01-24 16:12:11 -0300

    Exemplo de download de arquivo via FTP usando a biblioteca Paramiko

---
+++ main.py
@@ -0,0 +1,41 @@
+########## Acesso ao FTP usando a lib PARAMIKO https://www.paramiko.org/
+import paramiko
+import sys
+import datetime
+
+####| CREDENCIAIS DE ACESSO |####
+HOST = "ftp.dlptest.com"
+USER = "dlpuser"
+SENHA = "rNrKYTX9g7z3RgJRmxWuGHbeu"
+##### essa senha muda de tempos em tempos. acessar https://dlptest.com/ftp-test/ para conferir a senha atual
+
+
+####| PASTAS E ARQUIVO: ORIGEM e DESTINO |####
+dataHoje = datetime.datetime.now()
+dataOntem = dataHoje - datetime.timedelta(days=1)
+
+pastaDestino = "C:/pastadestino/"
+pastaFtp = "/home/pastaorigemftp/"
+arq = "NOME_ARQUIVO_" + dataOntem.strftime('%d_%m_%Y') ##o nome do arquivo muda dependendo do dia que o ftp for acessado
+####
+
+try:
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=HOST,username=USER,password=SENHA,allow_agent=False,look_for_keys=False)
+    sftp = client.open_sftp()
+    print('Conexão realizada com sucesso!\nBuscando arquivos:\n - ', arq+".csv")
+except:
+    print('\nOps ... tivemos um problema!\nErro na conexão: verifique as credenciais de acesso.')
+    sys.exit()
+####
+
+####| DOWNLOADS DO .CSV |####
+try:
+    sftp.get(pastaFtp+arq+".csv",pastaDestino+arq+".csv")
+    print('Arquivo',arq+".csv",' baixado com sucesso!')
+    sftp.close()
+except:
+    print('Ops ... tivemos um problema!')
+    print("\nArquivo: ",pastaDestino,arq+".csv não encontrado!")
+    sys.exit()
